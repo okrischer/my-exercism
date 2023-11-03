c27c26cf8d7427e860095287df7037e85e6a4af8
Oliver Krischer <oliver.krischer@gmail.com>
2023-11-03 22:59:10 +0100

    added mips: pop-count

--- kotlin/hamming/src/main/kotlin/Hamming.kt
+++ kotlin/hamming/src/main/kotlin/Hamming.kt
@@ -1,6 +1,6 @@
 object Hamming {
     fun compute(left: String, right: String): Int {
         require (left.length == right.length) { "left and right strands must be of equal length" }
-        return left.zip(right).count { it.first != it.second}
+        return left.zip(right).count { (l, r) -> l != r}
     }
 }
